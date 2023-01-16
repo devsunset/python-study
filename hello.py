0bfbdd8afdfecdb470caed79dba666e1073f76ae
devsunset <devsunset@gmail.com>
2023-01-16 10:41:56 +0900

    dev

---
+++ hello.py
@@ -0,0 +1,39 @@
+# print
+print('--------------------------')
+print("Hello World")
+print('hello')
+print('hello 안녕하세요')
+print('안녕','하세요','반갑습니다')
+print('안녕'+'하세요'+'반갑습니다')
+print("""안녕하세요
+오늘은 날씨가 좋네요""")
+print('''안녕하세요
+오늘은 날씨가 좋네요''')
+print("안녕하세요"\
+      "오늘은 날씨가 좋네요")
+a = 123
+b = "안녕하세요"
+print("a값:{} b값:{}".format(a,b))
+print(f"a값:{a} b값:{b}")
+
+# input
+print('--------------------------')
+# text = input()
+# print(text)
+
+# 변수 - 숫자형, 문자형, 소수점형, BOOL형
+print('--------------------------')
+i = 1
+j = 2
+k = 1+2
+print(k)
+l = '3'
+print(k+int(l))
+print(str(k)+l)
+
+e = 3.14
+f = 10
+print(e+f)
+print(i+j)
+print(float(i)+float(j))
+
