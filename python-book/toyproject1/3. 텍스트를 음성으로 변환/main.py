68b5dbefc447538cbc7c8ab3a346a75c94fe9a5c
devsunset <devsunset@gmail.com>
2023-01-18 09:28:23 +0900

    dev

---
+++ python-book/toyproject1/3. 텍스트를 음성으로 변환/main.py
@@ -0,0 +1,18 @@
+# pip install gtts
+from gtts import gTTS
+# pip install playsound
+from playsound import playsound
+import os
+
+# read_file = "hello world"
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)))
+file_path = 'readme.txt'
+with open(file_path, 'rt', encoding='UTF8') as f :
+    read_file = f.read()
+
+tts = gTTS(text=read_file, lang='ko')
+
+tts.save("readme.mp3")
+
+playsound("readme.mp3")
