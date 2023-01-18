3e359f50e7d4eb8e7437bdf8c309c6cb43f0c1a7
devsunset <devsunset@gmail.com>
2023-01-18 15:37:57 +0900

    dev

---
+++ python-book/toyproject1/7. 환율 변환기/main.py
@@ -0,0 +1,24 @@
+# pip install currencyconverter
+from currency_converter import CurrencyConverter
+import requests
+from bs4 import BeautifulSoup
+
+cc = CurrencyConverter()
+print(cc.currencies)
+
+cc = CurrencyConverter('http://www.ecb.europa.eu/stats/eurofxref/eurofxref.zip')
+print(cc.convert(1,'USD','KRW'))
+
+def get_exchange_rate():
+    headers = {
+        'User-Agent': 'Mozilla/5.0',
+        'Content-Type': 'text/html; charset=utf-8'
+    }
+
+    response = requests.get("https://www.kita.net/cmmrcInfo/ehgtGnrlzInfo/rltmEhgt.do", headers=headers)
+    content = BeautifulSoup(response.content, 'html.parser')
+    containers = content.find('div', {'class': 'tableSt st4 alc'})
+    print(containers)
+
+
+get_exchange_rate()
