6722b802f1a38c6dc5db66a96f464a75ac928eff
devsunset <devsunset@gmail.com>
2023-01-19 17:00:31 +0900

    dev

--- book-source/toyproject1/15.slack으로 스마트폰에 메시지보내기/main15-1.py
+++ book-source/toyproject1/15.slack으로 스마트폰에 메시지보내기/main15-1.py
@@ -2,6 +2,8 @@ import requests
 import json
 
 
+# slack_webhook_url = "https://hooks.slack.com/services/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"
+
 slack_webhook_url = "https://hooks.slack.com/services/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"
 
 def sendSlackWebhook(strText):
