4faa5a759ad600014f0cf3f89c40fcc3e3d1dccb
devsunset <devsunset@gmail.com>
2023-01-20 20:28:57 +0900

    dev

--- source/toyproject1/33.계산기 만들기(PYQT)/main.py
+++ source/toyproject1/33.계산기 만들기(PYQT)/main.py
@@ -2,8 +2,8 @@ import sys
 from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
-
-ui_path = r"33.계산기 만들기(PYQT)\계산기.ui"
+# pyqt designer 디자인
+ui_path = "계산기.ui"
 form_class = uic.loadUiType(ui_path)[0] 
 
 
@@ -31,9 +31,28 @@ class WindowClass(QMainWindow, form_class) :
 
         self.le_view.setEnabled(False)
         
+        self.text_value = ""
+        
     def btn_clicked(self):
         btn_value = self.sender().text()
-        print(btn_value)
+        if btn_value == 'C':
+            print("clear")
+            self.le_view.setText("0")
+            self.text_value = ""
+        elif btn_value == '=':
+            print("=")
+            try:
+                resultValue = eval(self.text_value.lstrip("0")) 
+                self.le_view.setText(str(resultValue))
+            except:
+                self.le_view.setText("error")
+        else:
+            if btn_value == 'X':
+                btn_value = '*'
+            self.text_value = self.text_value + btn_value
+            print(self.text_value)
+            self.le_view.setText(self.text_value)
+
 
 if __name__ == "__main__" :
     app = QApplication(sys.argv) 
