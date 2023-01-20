897a78dbcb5aeb8bb3c515cff1957890ae2938c1
devsunset <devsunset@gmail.com>
2023-01-20 11:21:19 +0900

    dev

--- book-source/toyproject1/23.사진에서 얼굴만 찾아 모자이크처리 (OpenCV)/main23-2.py
+++ book-source/toyproject1/23.사진에서 얼굴만 찾아 모자이크처리 (OpenCV)/main23-2.py
@@ -4,7 +4,7 @@ import cv2
 face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
 eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye.xml')
 
-ff = np.fromfile(r'23.사진에서 얼굴만 찾아 모자이크처리 (OpenCV)\샘플사진.jpg', np.uint8)
+ff = np.fromfile('test.jpg', np.uint8)
 img = cv2.imdecode(ff, cv2.IMREAD_UNCHANGED) 
 img = cv2.resize(img, dsize=(0, 0), fx=1.0, fy=1.0, interpolation=cv2.INTER_LINEAR)
 
