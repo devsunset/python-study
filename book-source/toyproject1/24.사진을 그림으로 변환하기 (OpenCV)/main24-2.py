5c0a4f03cbd24d469c4086f874f19f1b00910fef
devsunset <devsunset@gmail.com>
2023-01-20 11:25:10 +0900

    dev

--- book-source/toyproject1/24.사진을 그림으로 변환하기 (OpenCV)/main24-2.py
+++ book-source/toyproject1/24.사진을 그림으로 변환하기 (OpenCV)/main24-2.py
@@ -1,7 +1,8 @@
+# pip install opencv-python
 import numpy as np
 import cv2
 
-ff = np.fromfile(r'24.사진을 그림으로 변환하기 (OpenCV)\여행사진.jpg', np.uint8)
+ff = np.fromfile('test.jpg', np.uint8)
 img = cv2.imdecode(ff, cv2.IMREAD_UNCHANGED)
 img = cv2.resize(img, dsize=(0, 0), fx=1.0, fy=1.0, interpolation=cv2.INTER_LINEAR)
 
