a2def3c5e0d1b1bc5cb2ce0407bbae5129d4aff6
devsunset <devsunset@gmail.com>
2023-01-22 21:18:59 +0900

    dev

--- examples/source2/8.날씨정보 공문 만들기/main.py
+++ examples/source2/8.날씨정보 공문 만들기/main.py
@@ -12,10 +12,19 @@ os.chdir(os.path.dirname(os.path.abspath(__file__)))
 url = "http://www.kma.go.kr/wid/queryDFSRSS.jsp?zone=4139054000"
 response = requests.get(url)
 
+요청시간 = re.findall(r'<pubDate>(.+)</pubDate>',response.text)
+지역 = re.findall(r'<category>(.+)</category>',response.text)
 날씨정보 = re.findall(r'<wfKor>(.+)</wfKor>',response.text)
 시간 = re.findall(r'<hour>(.+)</hour>',response.text)
 온도 = re.findall(r'<temp>(.+)</temp>',response.text)
 습도 = re.findall(r'<reh>(.+)</reh>',response.text)
+
+print("요청시간: ",요청시간)
+print("지역: ",지역)
+print("날씨정보: ",날씨정보)
+print("시간: ",시간)
+print("온도: ",온도)
+print("습도: ",습도)
 
 #공문양식에서 읽어 문서번호, 발신일자, 수신, 제목 작성
 doc = Document('공문양식.docx')
