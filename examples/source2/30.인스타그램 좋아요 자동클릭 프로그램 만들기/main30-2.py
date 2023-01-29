e8f485d3fb2680dc8472b732e6deef0168982d42
devsunset <devsunset@gmail.com>
2023-01-29 16:46:02 +0900

    dev

--- examples/source2/30.인스타그램 좋아요 자동클릭 프로그램 만들기/main30-2.py
+++
@@ -1,8 +0,0 @@
-import pyautogui
-import os
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-pic_position = pyautogui.locateOnScreen("instar_pic.png", confidence=0.8) 
-click_position = pyautogui.center(pic_position)
-
-print(click_position)
